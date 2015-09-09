3a5024f66cc25fa7f6a6e1060e1506d2165c5dd1
Joe Reeve <joseph.reeve@googlemail.com>
2015-09-09 10:32:31 +0100

    Added visio svg namespace

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,19 +1,23 @@
 /* global console */
-import { hasConsole } from 'config/environment';
+import {
+    hasConsole
+}
+from 'config/environment';
 import Ractive from 'Ractive';
 import noop from 'utils/noop';
 
-var alreadyWarned = {}, log, printWarning, welcome;
+var alreadyWarned = {},
+    log, printWarning, welcome;
 
-if ( hasConsole ) {
-	let welcomeIntro = [
-		`%cRactive.js %c<@version@> %cin debug mode, %cmore...`,
-		'color: rgb(114, 157, 52); font-weight: normal;',
-		'color: rgb(85, 85, 85); font-weight: normal;',
-		'color: rgb(85, 85, 85); font-weight: normal;',
-		'color: rgb(82, 140, 224); font-weight: normal; text-decoration: underline;'
-	];
-	let welcomeMessage = `You're running Ractive <@version@> in debug mode - messages will be printed to the console to help you fix problems and optimise your application.
+if (hasConsole) {
+    let welcomeIntro = [
+        `%cSQUP Ractive, Things may behave differently %c<@version@> %cin debug mode, %cmore...`,
+        'color: rgb(114, 157, 52); font-weight: normal;',
+        'color: rgb(85, 85, 85); font-weight: normal;',
+        'color: rgb(85, 85, 85); font-weight: normal;',
+        'color: rgb(82, 140, 224); font-weight: normal; text-decoration: underline;'
+    ];
+    let welcomeMessage = `You're running Ractive <@version@> in debug mode - messages will be printed to the console to help you fix problems and optimise your application.
 
 To disable debug mode, add this line at the start of your app:
   Ractive.DEBUG = false;
@@ -32,91 +36,93 @@ Found a bug? Raise an issue:
 
 `;
 
-	welcome = () => {
-		let hasGroup = !!console.groupCollapsed;
-		console[ hasGroup ? 'groupCollapsed' : 'log' ].apply( console, welcomeIntro );
-		console.log( welcomeMessage );
-		if ( hasGroup ) {
-			console.groupEnd( welcomeIntro );
-		}
+    welcome = () => {
+        let hasGroup = !!console.groupCollapsed;
+        console[hasGroup ? 'groupCollapsed' : 'log'].apply(console, welcomeIntro);
+        console.log(welcomeMessage);
+        if (hasGroup) {
+            console.groupEnd(welcomeIntro);
+        }
 
-		welcome = noop;
-	};
+        welcome = noop;
+    };
 
-	printWarning = ( message, args ) => {
-		welcome();
+    printWarning = (message, args) => {
+        welcome();
 
-		// extract information about the instance this message pertains to, if applicable
-		if ( typeof args[ args.length - 1 ] === 'object' ) {
-			let options = args.pop();
-			let ractive = options ? options.ractive : null;
+        // extract information about the instance this message pertains to, if applicable
+        if (typeof args[args.length - 1] === 'object') {
+            let options = args.pop();
+            let ractive = options ? options.ractive : null;
 
-			if ( ractive ) {
-				// if this is an instance of a component that we know the name of, add
-				// it to the message
-				let name;
-				if ( ractive.component && ( name = ractive.component.name ) ) {
-					message = `<${name}> ${message}`;
-				}
+            if (ractive) {
+                // if this is an instance of a component that we know the name of, add
+                // it to the message
+                let name;
+                if (ractive.component && (name = ractive.component.name)) {
+                    message = `<${name}> ${message}`;
+                }
 
-				let node;
-				if ( node = ( options.node || ( ractive.fragment && ractive.fragment.rendered && ractive.find( '*' ) ) ) ) {
-					args.push( node );
-				}
-			}
-		}
+                let node;
+                if (node = (options.node || (ractive.fragment && ractive.fragment.rendered && ractive.find('*')))) {
+                    args.push(node);
+                }
+            }
+        }
 
-		console.warn.apply( console, [ '%cRactive.js: %c' + message, 'color: rgb(114, 157, 52);', 'color: rgb(85, 85, 85);' ].concat( args ) );
-	};
+        console.warn.apply(console, ['%cRactive.js: %c' + message, 'color: rgb(114, 157, 52);', 'color: rgb(85, 85, 85);'].concat(args));
+    };
 
-	log = function () {
-		console.log.apply( console, arguments );
-	};
+    log = function() {
+        console.log.apply(console, arguments);
+    };
 } else {
-	printWarning = log = welcome = noop;
+    printWarning = log = welcome = noop;
 }
 
-function format ( message, args ) {
-	return message.replace( /%s/g, () => args.shift() );
+function format(message, args) {
+    return message.replace(/%s/g, () => args.shift());
 }
 
-function fatal ( message, ...args ) {
-	message = format( message, args );
-	throw new Error( message );
+function fatal(message, ...args) {
+    message = format(message, args);
+    throw new Error(message);
 }
 
-function logIfDebug () {
-	if ( Ractive.DEBUG ) {
-		log.apply( null, arguments );
-	}
+function logIfDebug() {
+    if (Ractive.DEBUG) {
+        log.apply(null, arguments);
+    }
 }
 
-function warn ( message, ...args ) {
-	message = format( message, args );
-	printWarning( message, args );
+function warn(message, ...args) {
+    message = format(message, args);
+    printWarning(message, args);
 }
 
-function warnOnce ( message, ...args ) {
-	message = format( message, args );
+function warnOnce(message, ...args) {
+    message = format(message, args);
 
-	if ( alreadyWarned[ message ] ) {
-		return;
-	}
+    if (alreadyWarned[message]) {
+        return;
+    }
 
-	alreadyWarned[ message ] = true;
-	printWarning( message, args );
+    alreadyWarned[message] = true;
+    printWarning(message, args);
 }
 
-function warnIfDebug () {
-	if ( Ractive.DEBUG ) {
-		warn.apply( null, arguments );
-	}
+function warnIfDebug() {
+    if (Ractive.DEBUG) {
+        warn.apply(null, arguments);
+    }
 }
 
-function warnOnceIfDebug () {
-	if ( Ractive.DEBUG ) {
-		warnOnce.apply( null, arguments );
-	}
+function warnOnceIfDebug() {
+    if (Ractive.DEBUG) {
+        warnOnce.apply(null, arguments);
+    }
 }
 
-export { fatal, log, logIfDebug, warn, warnOnce, warnIfDebug, warnOnceIfDebug, welcome };
+export {
+    fatal, log, logIfDebug, warn, warnOnce, warnIfDebug, warnOnceIfDebug, welcome
+};
